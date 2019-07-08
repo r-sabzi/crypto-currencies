2ac3d5a4847f21cc08f54b093269bd77384c1063
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-08 17:50:31 +0430

    make Table a seperate component, add prop-types

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { handleResponse } from './../../helpers'
 import { API_URL } from './../../config'
-import './Table.css'
 import Loading from './../common/Loading'
+import Table from './Table';
 
 
 
@@ -18,16 +18,17 @@ export default class List extends Component {
     componentDidMount() {
         this.setState({loading:true})
         fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
-        .then(handleResponse)
+            .then(handleResponse)
         .then((data) => {
             this.setState({
                 currencies: data.currencies,
                 loading: false
             })
         })
-        .catch((error) => {
+            .catch((error) => {
+              
             this.setState({
-                error: error.errorMessage,
+                error: error.errorMessage||error.toString(),
                 loading: false
             })
         });
@@ -52,38 +53,8 @@ export default class List extends Component {
         if (error) {
             return <span className="error">{error}</span>
 }
-
         return (
-            <div className='Table-container'>
-       
-                <table className="Table">
-                    <thead className="Table-head">
-                        <tr>
-                            <th>Cryptocurrency</th>
-                            <th>Price</th>
-                            <th>Market Cap</th>
-                            <th>24H Change</th>
-                        </tr>
-                    </thead>
-                    <tbody className="Table-body">
-                    {
-                    currencies.map(currency =>
-                        <tr key={currency.id}>
-                            <td><span className="Table-rank">{currency.rank}</span>{currency.name}</td>
-                            <td><span className="Table-dollar">${currency.price}</span></td>
-                            <td><span className="Table-dollar">${currency.marketCap}</span></td>
-                            <td>
-                                {this.renderChangePercent(currency.percentChange24h)}
-                            </td>
-                            
-                        </tr>)
-                
-                }
-                    </tbody>
-                </table>
-
-
-            </div>
+            <Table currencies={currencies} renderChangePercent={this.renderChangePercent}/>
         )
     }
 }
